9b2146cc0b4f725e101d1801fcd36bba8f01d1a9
Nagarjunak4 <nagarjuna.kottam@certifyglobal.com>
2020-12-21 16:46:45 -0500

    Removed Bin and obj folder

--- OnePayMiuraXamarin/OnepayMiuraBindingLibrary/obj/Release/generated/src/Com.Miurasystems.Mpi.Aspects.LoggingAnnotationAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Com.Miurasystems.Mpi.Aspects {
-
-	[global::Android.Runtime.Annotation ("com.miurasystems.mpi.aspects.LoggingAnnotation")]
-	public partial class LoggingAnnotationAttribute : Attribute
-	{
-	}
-}
--- OnePayMiuraXamarin/OnepayMiuraBindingLibrary/obj/Release/generated/src/Org.Aspectj.Internal.Lang.Annotation.AjcPrivilegedAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Org.Aspectj.Internal.Lang.Annotation {
-
-	[global::Android.Runtime.Annotation ("org.aspectj.internal.lang.annotation.ajcPrivileged")]
-	public partial class AjcPrivilegedAttribute : Attribute
-	{
-	}
-}
